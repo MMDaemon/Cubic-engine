04b168aa6971e94ea8405e9d38ad20f5d54f3661
MMDaemon <mettenleitermatthias@web.de>
2016-05-30 12:50:14 +0200

    Added Cubic-engine project and create first window with OpenTK

---
+++ Cubic-engine/MyApplication.cs
@@ -0,0 +1,32 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Cubic_engine
+{
+	class MyApplication
+	{
+		private GameWindow gameWindow = new GameWindow();
+
+		static void Main(string[] args)
+		{
+			var app = new MyApplication();
+			app.Run();
+		}
+
+		private MyApplication()
+		{
+			gameWindow.RenderFrame += game_RenderFrame;
+		}
+
+		private void Run()
+		{
+			gameWindow.Run(60);
+		}
+
+		private void game_RenderFrame(object sender, FrameEventArgs e)
+		{
+			GL.Clear(ClearBufferMask.ColorBufferBit);
+			gameWindow.SwapBuffers();
+		}
+	}
+}
